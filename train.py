51fa9c871d365059a4055a6c4d801a0d942d3502
RaneemF <113118208+RaneemF@users.noreply.github.com>
2025-05-21 03:08:42 +0300

    Create train.py

---
+++ train.py
@@ -0,0 +1,2 @@
+The training code and YOLOv5 implementation used in this project can be accessed through the following link:
+"https://colab.research.google.com/drive/148Ru8J-MNLYdfU7xUrHNDy9DNdLpoKNU?usp=shar
